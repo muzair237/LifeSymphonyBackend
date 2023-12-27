a4912f7cbcbe03a94c605cec07ff222b4b325a8d
Uzair <uzair.ejaz2001@gmail.com>
2023-12-27 22:59:25 +0500

    feat: added profilePicture api

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,19 @@
 const express = require("express");
-const { SignUp, Login, UpdateProfile, sendOTP, confirmOTP, updatePassword } = require("../controllers/UserController");
+const multer = require("multer");
+const { SignUp, Login, UpdateProfile, sendOTP, confirmOTP, updatePassword, profilePicture } = require("../controllers/UserController");
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'assets/profilePictures/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueFilename = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueFilename);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 router.post('/signUp', SignUp);
 router.post('/login', Login);
@@ -8,5 +21,6 @@ router.put('/updateProfile/:userId', UpdateProfile);
 router.post('/sendOtp', sendOTP);
 router.post('/confirmOtp', confirmOTP);
 router.post('/updatePassword', updatePassword);
+router.post('/profilePicture', upload.single('profilePicture'), profilePicture)
 
 module.exports = router;
