ecc3a4619674d7190466a9f0ae320eacce942783
Uzair <uzair.ejaz2001@gmail.com>
2023-12-31 20:53:26 +0500

    feat: prof pic added

--- src/utils/funcHelper.js
+++ src/utils/funcHelper.js
@@ -1,9 +1,9 @@
-module.exports.getRandomNumber = function(endNumber) {
+module.exports.getRandomNumber = function(count) {
     const today = new Date();
     const seed = today.toISOString().slice(0, 10);
     const seedInt = parseInt(seed.replace(/-/g, ''), 10);
     const random = (seedInt * 9301 + 49297) % 233280;
-    const randomNumber = Math.floor(random / 233280 * endNumber) + 1;
+    const randomNumber = Math.floor(random / 233280 * count) + 1;
     return randomNumber;
   };
   
